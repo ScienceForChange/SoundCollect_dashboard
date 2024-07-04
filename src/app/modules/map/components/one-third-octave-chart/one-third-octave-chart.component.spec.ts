2506c9fb460084a03325b59efb0658ac4ac8a88a
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-07-04 12:32:23 +0200

    Chart add

---
+++ src/app/modules/map/components/one-third-octave-chart/one-third-octave-chart.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OneThirdOctaveChartComponent } from './one-third-octave-chart.component';
+
+describe('OneThirdOctaveChartComponent', () => {
+  let component: OneThirdOctaveChartComponent;
+  let fixture: ComponentFixture<OneThirdOctaveChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [OneThirdOctaveChartComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(OneThirdOctaveChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
