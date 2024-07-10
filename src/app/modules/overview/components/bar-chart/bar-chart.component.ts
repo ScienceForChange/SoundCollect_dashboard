c1178b82ce33b006c4c6fe45a5acd6793ccb9405
TomasTubertSFC <tomas.tubert@scienceforchange.eu>
2024-07-10 17:15:54 +0200

    Updated the filters logic

--- src/app/modules/overview/components/bar-chart/bar-chart.component.ts
+++ src/app/modules/overview/components/bar-chart/bar-chart.component.ts
@@ -40,7 +40,14 @@ export class BarChartComponent implements OnInit, AfterViewInit {
     WEEK: 'week',
     MONTH: 'month',
     YEAR: 'year',
-  }
+  };
+  private daysOfWeek = Array.from({ length: 7 }, (_, i) => {
+    const today = new Date();
+    const firstDayOfWeek = new Date(
+      today.setDate(today.getDate() - today.getDay() + 1 + i)
+    );
+    return firstDayOfWeek.toLocaleDateString('ca-ES', { weekday: 'long' });
+  });
   private observations: ObservationsDataChart[] = [];
   private obsFiltered: ObservationsDataChart[] = [];
   public today: Date = new Date();
@@ -57,9 +64,9 @@ export class BarChartComponent implements OnInit, AfterViewInit {
     daysFilter: new FormControl([this.lastDay30, new Date()], []),
   });
 
-
   ngOnInit(): void {
     echarts.use([GridComponent, BarChart, CanvasRenderer]);
+    // console.log('this.daysOfWeek', this.daysOfWeek)
     this.filtersForm.valueChanges.subscribe(
       (values: { daysFilter: [Date, Date | null] }) => {
         const haveTwoDaysSelected = values.daysFilter[1] !== null;
@@ -70,40 +77,40 @@ export class BarChartComponent implements OnInit, AfterViewInit {
             if (isBeforeToday && isAfterLastDay30) return true;
             return false;
           });
-          this.updateChart(this.obsFiltered);
+          const dataXaxis = this.obsFiltered.map((obs) => obs.date)
+          const dataSerie = this.obsFiltered.map((obs) => obs.count)
+
+          this.updateChart(dataXaxis,dataSerie);
           this.timeFilterSelected = this.timesFilter.DELETE;
         }
       }
     );
   }
 
-  private updateChart(observations: ObservationsDataChart[]) {
+  private updateChart(xAxis: string[],serieData:number[]) {
     this.options = {
       xAxis: {
         type: 'category',
-        data: observations.map((obs) => obs.date),
-      },
-      yAxis: {
-        name: this.translate.instant('overview.barChart.yAxis'),
-        nameLocation: 'middle',
-        nameGap: 35,
-        type: 'value'
+        data: xAxis,
       },
       series: [
         {
-          data: observations.map((obs) => obs.count),
+          data: serieData,
           type: 'bar',
         },
       ],
     };
-    this.options && this.myChart.setOption(this.options);
+    this.myChart.setOption(this.options);
   }
 
   private currentWeek(date: Date) {
-    let yearStart = new Date(date.getFullYear(), 0, 1);
-    let today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-    let dayOfYear = (today.valueOf() - yearStart.valueOf() + 1) / 86400000;
-    return Math.ceil(dayOfYear / 7);
+    // let yearStart = new Date(date.getFullYear(), 0, 1);
+    // let today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    // let dayOfYear = (today.valueOf() - yearStart.valueOf() + 1) / 86400000;
+    const day = new Intl.DateTimeFormat('ca', { weekday: 'long' }).format(date);
+    return day;
+    console.log('dayOfYear', day);
+    // return Math.ceil(dayOfYear / 7);
   }
 
   public timeFilter(filter: string) {
@@ -116,37 +123,64 @@ export class BarChartComponent implements OnInit, AfterViewInit {
       return false;
     });
     let filteredObsByTime = obsFiltered;
-    if(filter !== this.timesFilter.DELETE){
-      //Group obs selected by the time selected
-      const groupByTime = obsFiltered.reduce(
-        (acc: { [key: number]: any[] }, obs) => {
-          const day = obs.completeDay;
-          const time =
-            filter === 'week'
-              ? this.currentWeek(day)
-              : filter === 'month'
-              ? day.getMonth()
-              : day.getFullYear();
-          if (!acc[time]) {
-            acc[time] = [obs];
-          }
-          acc[time].push(obs);
-          return acc;
-        },
-        {}
-      );
-      //Create an array with the grouped obs with the structured data needed
-      filteredObsByTime = Object.values(groupByTime).map((timeObs) => {
-        return {
-          date: timeObs[0].date,
-          obs: timeObs,
-          count: timeObs.length,
-          completeDay: timeObs[0].completeDay,
-        };
-      });
+    let dataXaxis: string[]= [];
+    let dataSerie: number[] = [];
+    if (filter !== this.timesFilter.DELETE) {
+      if (filter === this.timesFilter.WEEK) {
+        const daysOfWeekSelected = obsFiltered.reduce((acc, curr) => {
+          if (acc.includes(this.currentWeek(curr.completeDay))) return acc;
+          return [...acc, this.currentWeek(curr.completeDay)];
+        }, []);
+        const series = daysOfWeekSelected.map((day) => {
+          return obsFiltered.filter(
+            (obs) => this.currentWeek(obs.completeDay) === day
+          ).length;
+        });
+        dataXaxis = this.daysOfWeek;
+        dataSerie = this.daysOfWeek.map((count) => {
+          const index = dataXaxis.indexOf(count);
+          return series[index];
+        });
+      }
+      if (filter === this.timesFilter.MONTH) {
+        const months: number[] = obsFiltered.reduce((acc, curr) => {
+          const month = curr.completeDay.getMonth();
+          if (acc.includes(month)) return acc;
+          return [...acc, month];
+        }, []);
+        dataXaxis = months.map((month) =>
+          new Intl.DateTimeFormat('ca-ES', { month: 'long' }).format(
+            new Date(0, month)
+          )
+        );
+        dataSerie = months.map((month) => {
+          return obsFiltered.filter((obs) => {
+            const obsMonth = obs.completeDay.getMonth();
+            return obsMonth === month;
+          }).length;
+        });
+      }
+      if(filter === this.timesFilter.YEAR){
+        dataXaxis = obsFiltered.reduce((acc,curr) => {
+          const year = curr.completeDay.getFullYear()
+          if (acc.includes(year)) return acc;
+          return [...acc, year];
+        },[])
+        dataSerie = dataXaxis.map((year) => {
+          return obsFiltered.filter((obs) => {
+            const obsMonth = obs.completeDay.getFullYear();
+            return obsMonth === +year;
+          }).length;
+        });
+      }
+    } else {
+      dataXaxis = filteredObsByTime.map((obs) => obs.date)
+      dataSerie = filteredObsByTime.map((obs) => obs.count)
     }
+    console.log('dataXaxis', dataXaxis);
+    console.log('dataSerie', dataSerie);
     this.obsFiltered = filteredObsByTime;
-    this.updateChart(this.obsFiltered);
+    this.updateChart(dataXaxis, dataSerie);
     this.timeFilterSelected = filter;
   }
 
@@ -182,11 +216,11 @@ export class BarChartComponent implements OnInit, AfterViewInit {
             rotate: 45, // Optional: you can rotate labels to prevent overlapping
           },
         },
-        yAxis:{
+        yAxis: {
           name: this.translate.instant('overview.barChart.yAxis'),
           nameLocation: 'middle',
           nameGap: 35,
-          type: 'value'
+          type: 'value',
         },
         series: [
           {
